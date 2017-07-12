c5bd1aa3bb690212a7d5821e3c77734dcc485864
Nic McDonald <n.mcdonald83@gmail.com>
2017-07-12 12:03:56 -0600

    using new location of SpinLock.h in libdes

--- src/main.cc
+++ src/main.cc
@@ -28,7 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <des/SpinLock.h>
+#include <des/util/SpinLock.h>
 #include <prim/prim.h>
 #include <tclap/CmdLine.h>
 
